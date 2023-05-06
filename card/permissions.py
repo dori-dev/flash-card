a6f17862362ac455fbb8eddd119bff0a5589cd9e
dori-dev <mr.dori.dev@gmail.com>
2023-05-06 20:59:32 +0330

    add is authenticated permission before object permission

--- card/permissions.py
+++ card/permissions.py
@@ -2,9 +2,15 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsOwnerOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.method in SAFE_METHODS or
+            request.user.is_authenticated and
+            request.user
+        )
+
     def has_object_permission(self, request, view, obj):
         return bool(
             request.method in SAFE_METHODS or
-            request.user and
             obj.user == request.user
         )
